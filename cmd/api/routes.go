b932b988d21fd26456449467a9f1ddd2f7e26296
Joel Ical <2005113038@ub.edu.bz>
2022-09-10 15:29:19 -0600

    installed thirdparty router

---
+++ cmd/api/routes.go
@@ -0,0 +1,20 @@
+//Filename: cmd/api/routes.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// create a method that returns a http router
+func (app *application) routes() *httprouter.Router {
+	//Create a new httrouter router instance
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/entries", app.createEntryHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/entries/:id", app.showEntryHandler)
+
+	return router
+}
