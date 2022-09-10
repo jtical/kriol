2d93a78d3165503890002fee88d2ee6957ee494f
Joel Ical <2005113038@ub.edu.bz>
2022-09-10 16:33:43 -0600

    using json format to display responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,13 +3,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // create a healthcheck handler
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	//create a map to hold healthcheck data
+	data := map[string]string{
+		"status":      "available",
+		"enviornment": app.config.env,
+		"version":     version,
+	}
+	//convert our map into a json object
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounter a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	//a new line to byte slices to make viewing on the terminal easier
+	js = append(js, '\n')
+	// specify that we will server our responses using JSON
+	w.Header().Set("Content-Type", "application/json")
+	//we write out the []byte slices conating the json response boday
+	w.Write(js)
+
 }
